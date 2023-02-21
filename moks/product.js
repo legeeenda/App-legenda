8f21c30d00fb5876c2ff99bc097105067f85434b
legeeenda <119280894+legeeenda@users.noreply.github.com>
2023-02-21 15:11:57 +0300

    moks

---
+++ moks/product.js
@@ -0,0 +1,31 @@
+const products = [
+    {
+        photoSrc : './img/days.jpg',
+        tittle : 'Vip',
+        desc : 'Дается закрытый доступ!',
+        price : '900 рублей'
+    },
+
+    {
+        photoSrc : './img/hw.jpg',
+        tittle : 'Hwid',
+        desc : 'Меняет данные компьютера!',
+        price : '2000 рублей'
+    },
+
+    {
+        photoSrc : './img/Acc.png',
+        tittle : 'Account',
+        desc : 'Аккаунты от Rust',
+        price : '2300 рублей'
+    },
+
+    {
+        photoSrc : './img/setting.png',
+        tittle : 'Настройка',
+        desc : 'Настройка проекта!',
+        price : '3000 рублей'
+    }
+]
+
+module.exports = products;
